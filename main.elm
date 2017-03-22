cf2e59215c306aac90144b97bd133bd6db593b52
matija <vodokotlic.vudukotlic@gmail.com>
2017-03-22 16:53:54 +0100

    arrowKeys

--- main.elm
+++ main.elm
@@ -5,7 +5,7 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time)
 import AnimationFrame
-import Keyboard exposing (..)
+import Keyboard
 
 
 main =
@@ -28,11 +28,9 @@ type alias Model =
     }
 
 
-defaultBeatCount = 2
+defaultBeatCount = 1
 
 defaultBpm = 60
-
-br = 1000
 
 init : ( Model, Cmd Msg )
 init =
@@ -55,14 +53,28 @@ init =
 type Msg
     = TimeUpdate Time
     | BpmUpdate String
---    | BeatUpdate String
+    | BeatUpdate String
     | StartRunning
     | StopRunning
+    | KeyMsg Keyboard.KeyCode
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        KeyMsg code ->
+            case code of
+                37 ->
+                    ( (updateBeatCount model (model.beatCount - 1)), Cmd.none )
+                38 ->
+                    --( {model | bpm = model.bpm + 1 }, Cmd.none )
+                    ( (updateBpm model (model.bpm + 1)), Cmd.none )
+                39 ->
+                    ( (updateBeatCount model (model.beatCount + 1)), Cmd.none )
+                40 ->
+                    ( (updateBpm model (model.bpm - 1)), Cmd.none )
+                _ ->
+                    (model, Cmd.none)
         TimeUpdate deltaT ->
             ( (updateColor model deltaT), Cmd.none )
 
@@ -79,18 +91,18 @@ update msg model =
             in
                 ( (updateBpm model intBpm), Cmd.none )
 
-{--        BeatUpdate newBeatCount ->
+        BeatUpdate newBeatCount ->
             let
                 beatCount =
                     case String.toInt newBeatCount of
                         Err msg ->
-                            defaultBeatCount
+                            0
 
                         Ok converted ->
                             converted
             in
                 ( (updateBeatCount model beatCount), Cmd.none )
---}
+
         StartRunning ->
             ( { model | running = True }, Cmd.none )
 
@@ -102,8 +114,10 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     case model.running of
         True ->
-            AnimationFrame.diffs TimeUpdate
-
+            Sub.batch
+                [AnimationFrame.diffs TimeUpdate
+                ,Keyboard.downs KeyMsg
+                ]
         False ->
             Sub.none
 
@@ -111,24 +125,24 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ {--div []
+        [ div []
             [ label []
                 [ Html.text "BPM"
                 , input [ type_ "text", value (toString model.bpm), onInput BpmUpdate ] []
                 ]
             ]
-{--        ,--} div []
+            ,div []
             [ label []
                 [ Html.text "Beat Count"
                 , input [ type_ "text", value (toString model.beatCount), onInput BeatUpdate ] []
                 ]
-            ] FALI ZAREZ
---}
-         div [] [ startButton model.running ]
+            ]
+
+        , div [] [ startButton model.running ]
         , svg [ viewBox "0 0 100 100", width "800px" ] --(buildFace model)
         [ circle [ cx "50", cy "50", r "45", fill model.fillColor ] []
         ]
-
+{--
         , div []
         [ Html.h3 [] [ text (toString model.timeDiff) ]]
         , div []
@@ -147,23 +161,28 @@ startButton running =
 
 updateBpm : Model -> Int -> Model
 updateBpm model newBpm =
-    { model | bpm = newBpm }
+    let
+        interval = 60000 // (model.beatCount * newBpm)
+    in
+        { model | bpm = newBpm , interval = interval}
+
+updateBeatCount : Model -> Int -> Model
+updateBeatCount model beatCount =
+    let
+        interval = 60000 // (beatCount * model.bpm)
+    in
+        { model | beatCount = beatCount, interval = interval }
+
+
 
 
 updateColor : Model -> Time -> Model
 updateColor model deltaT =
     let
         br = model.interval
-        --br1 : Time
-        --br1 = model.interval * Time.millisecond
         ms = br
         ms1 = (br - 50)
         mil = round (Time.inMilliseconds model.increment)
-        fillColor =
-            if model.fillColor == "#0B79CE" then
-                "#fe57a1"
-            else
-                "#0B79CE"
     in
 
     if mil < ms1 then
